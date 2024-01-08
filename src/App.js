511e298a29bfc5a99d9da30915d2613d940d450b
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-08 11:01:32 +0100

    add children prop

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me first</Button>
+      <Button>Then me</Button>
+      <Button>I am the third button</Button>
+      <Button>And another button</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
